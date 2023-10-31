0f35483b542567073d329bbfe28a60c755186ce9
Ziad <ziadlabban@hotmail.co.uk>
2023-10-31 11:37:17 +0000

    step 2

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Premier league table')
+
+  const responseObj = {
+    status: "success",
+    data: {
+    msg: "This is a message from the response object"
+    }
+}
+
+// Sending a response
+return res.status(299).json(responseObj);
 });
+
+app.get('/teams', async function (req, res) {
+    // create a variable which awaits
+    res.send('Premier league table');
+  });
 
 app.listen(PORT, function () {
     console.log(`Server is now listening on http://localhost:${PORT}`);
