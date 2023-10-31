48a41b1f52a914287824349a5af4ae5d62d25863
Ziad <ziadlabban@hotmail.co.uk>
2023-10-31 12:02:41 +0000

    step 3

--- app.js
+++ app.js
@@ -2,24 +2,44 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+import {
+  getTeams,
+  getTeamByID,
+  // addTeam,
+  // editTeam,
+  // deleteTeam,
+} from "./team.js";
+
+//built in middleware
+app.use(express.json()); // Parses the body data and make it available on req.body
+
+
 app.get('/', function (req, res) {
   res.send('Premier league table')
-
-  const responseObj = {
-    status: "success",
-    data: {
-    msg: "This is a message from the response object"
-    }
-}
-
-// Sending a response
-return res.status(299).json(responseObj);
 });
 
-app.get('/teams', async function (req, res) {
-    // create a variable which awaits
-    res.send('Premier league table');
+
+  app.get("/teams", async function(req, res) {
+    const allTeams = await getTeams();
+    // return the array of all the quotes
+    // prepare a response
+    const responseObj = {
+      status: "success",
+      data: allTeams,
+    };
+  return res.status(200).json(responseObj);
   });
+
+  app.get("/teams/:id", async function(req, res) {
+    // store id parameter as a variable named id
+    const teamsId = req.params.id;
+    const team = await getTeamByID(teamsId);
+    const responseObj = {
+      status: "success",
+      data: team,
+    };
+  return res.status(200).json(responseObj);
+  })
 
 app.listen(PORT, function () {
     console.log(`Server is now listening on http://localhost:${PORT}`);
--- team.js
+++ team.js
@@ -3,3 +3,9 @@ import path from "node:path";
 import { v4 as uuidv4 } from "uuid";
 
 const filePath = path.resolve(process.cwd(), "teams.json");
+
+export async function getTeams() {
+    const teamsJSON = await fs.readFile(filePath, "utf-8");
+    const teams = JSON.parse(teamsJSON);
+    return teams;
+  }
